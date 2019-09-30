07185dec41d1ff44c3476ab3c6bad3e2338ed3b7
KSYeon2016 <kg000003@gmail.com>
2019-09-30 17:18:06 +0900

    task, subtask 이름 변경 기능

--- src/app/subtask/subtask.component.ts
+++ src/app/subtask/subtask.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 import { SubTask } from 'app/model/subtask';
 
 @Component({
@@ -10,9 +10,39 @@ export class SubtaskComponent implements OnInit {
   @Input()
   subTask: SubTask;
 
-  constructor() { }
+  editingsubTask = false;
+  currentTitle: string;
 
-  ngOnInit() {
+  constructor(private el: ElementRef) { }
+
+  ngOnInit() { }
+
+  updatesubTask() {
+    if (!this.subTask.title || this.subTask.title.trim() === '') {
+      this.subTask.title = this.currentTitle;
+    }
+
+    this.editingsubTask = false;
+  }
+
+  blurOnEnter(event) {
+    if (event.keyCode === 13) {
+      event.target.blur();
+    } else if (event.keyCode === 27) {
+      this.subTask.title = this.currentTitle;
+      this.editingsubTask = false;
+    }
+  }
+
+  editsubTask() {
+    this.editingsubTask = true;
+    this.currentTitle = this.subTask.title;
+
+    let textArea = this.el.nativeElement.getElementsByTagName('textarea')[0];
+
+    setTimeout(function() {
+      textArea.focus();
+    }, 0);
   }
 
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { Task } from 'app/model/task';
 import { SubTask } from 'app/model/subtask';
 import { ActivatedRoute } from '@angular/router';
 import { TrelloService } from 'app/services/trello.service';
+import { Board } from 'app/model/board';
 
 @Component({
   selector: 'app-task',
@@ -19,21 +20,69 @@ export class TaskComponent implements OnInit {
   @Output()
   public onAddSubTask: EventEmitter<SubTask>;
 
+  boards: Board[];
+  board: Board = new Board;
   addSubTaskText: string;
+  editingtask = false;
+  currentTitle: string;
 
-  constructor(private _route: ActivatedRoute, private _boardService: TrelloService) { 
+  constructor(private el: ElementRef, private _route: ActivatedRoute, private _boardService: TrelloService) { 
     this.onAddSubTask = new EventEmitter();
   }
 
   ngOnInit() {
-  }
-
-  addSubTask() {
-    this.subTasks = this.subTasks || [];
-    const newSubTask = <SubTask>{
-      title: this.addSubTaskText
+    const boardId = this._route.snapshot.params['id'];
+    if (boardId != undefined) {
+      this.boards = this._boardService.Boards;
+      console.log(this.boards);
+      for (const v of this.boards) {
+        if (v.id == boardId) {
+          this.board = v;
+          break;
+        }
+      }
     };
-    this.onAddSubTask.emit(newSubTask);
   }
 
+  clearAddsubTask() {
+    this.addSubTaskText = '';
+  }
+
+  updatetaskOnBlur() {
+    if (this.editingtask) {
+      this.updatetask();
+      this.clearAddsubTask();
+    }
+  }
+
+  cleadAddtask() {
+    this.task.title = this.currentTitle;
+    this.editingtask = false;
+  }
+
+  updatetask() {
+    if (this.task.title && this.task.title.trim() !== '') {
+      this.editingtask = false;
+    } else {
+      this.cleadAddtask();
+    }
+  }
+
+  addtaskOnEnter(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      this.updatetask();
+    } else if (event.keyCode === 27) {
+      this.cleadAddtask();
+    }
+  }
+
+  edittask() {
+    this.currentTitle = this.task.title;
+    this.editingtask = true;
+    const input = this.el.nativeElement
+      .getElementsByClassName('task-header')[0]
+      .getElementsByTagName('input')[0];
+
+    setTimeout(function () { input.focus(); }, 0);
+  }
 }
