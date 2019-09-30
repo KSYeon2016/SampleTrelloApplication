810b109bb93086678bfc4b7911dc17e3bc90cb1e
KSYeon2016 <kg000003@gmail.com>
2019-09-30 11:01:15 +0900

    ch05 - p.198 홈페이지 데이터 초기화 (HTTP 서비스 호출)

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Board } from 'app/model/board';
+import { TrelloService } from 'app/services/trello.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+// OnInit: 컴포넌트를 초기화하고 데이터 바인딩된 프로퍼티를 표시
+export class HomepageComponent implements OnInit {  
+  boards: Board[] = Array();
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private _trelloService: TrelloService) { }
 
   ngOnInit() {
+    this.boards.push(this._trelloService.seedData());
   }
 
 }
